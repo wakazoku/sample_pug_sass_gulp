02f1d04b786e4247c08281083d32327169149f7a
Wakatabi <wakazoku@gmail.com>
2018-12-16 19:28:01 +0900

    タスクの書き方をv4に合わせる

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
-## 環境構築
+## ▼環境構築
+※多分いけると思う
+### 1. gulp の cli ツールをインストールする
 
-この記事が詳しい。  
-https://ics.media/entry/3290
+```
+npm install --global gulp-cli
+```
 
-### 1. npm インストールする
-``` node.js
+### 2. npm インストールする
+
+```node.js
 npm i
 ```
 
-### 2. 実行する
-``` node.js
-npx gulp
+## ▼gulp
+
+バージョン 4 なので基本的には公式ドキュメントを参考にされたし。  
+https://gulpjs.com/  
+※古いバージョンの書き方だと動かないことが多い。
+
+### SCSSをコンパイルするタスク
+```node.js
+gulp
 ```
-watch式があるので対象ファイルに変化があると自動的にコンパイルされる
+もしくは
+```node.js
+gulp css
+```
+ファイルの変更をウォッチする場合は
+```node.js
+gulp watch
+```
+### 使用可能なタスクを確認する
+```node.js
+gulp --tasks
+```
+こんな感じで表示される
+```
+$ gulp --tasks
+[19:25:44] Tasks for ~/dev/pug_sass_gulp/gulpfile.js
+[19:25:44] ├── default
+[19:25:44] ├── css
+[19:25:44] └── watch
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-// gulpプラグインの読み込み
-const gulp = require("gulp");
+// const gulp = require("gulp");
+const { src, dest, watch } = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
 
-// style.scssの監視タスクを作成する
-gulp.task("default", function() {
-  // ★ style.scssファイルを監視
-  return gulp.watch("css/style.scss", function() {
-    // style.scssの更新があった場合の処理
+function css() {
+  return (
+    src("src/sass/*.+(scss|sass)")
+      // Sassのコンパイルを実行
+      .pipe(
+        sass({
+          outputStyle: "expanded"
+        })
+          // Sassのコンパイルエラーを表示
+          // (これがないと自動的に止まってしまう)
+          .on("error", sass.logError)
+      )
+      // cssフォルダー以下に保存
+      .pipe(dest("dist/css"))
+  );
+}
 
-    // style.scssファイルを取得
-    return (
-      gulp
-        .src("css/style.scss")
-        // Sassのコンパイルを実行
-        .pipe(
-          sass({
-            outputStyle: "expanded"
-          })
-            // Sassのコンパイルエラーを表示
-            // (これがないと自動的に止まってしまう)
-            .on("error", sass.logError)
-        )
-        // cssフォルダー以下に保存
-        .pipe(gulp.dest("css"))
-    );
-  });
-});
+function watchCss() {
+  watch("src/sass/*.+(scss|sass)", css);
+}
+
+exports.default = css;
+exports.css = css;
+exports.watch = watchCss;
